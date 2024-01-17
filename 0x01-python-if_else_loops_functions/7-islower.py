cbd621cdf4f995f95f937fa30e167d38bd15c085
Rsilimwe <Silimwerodgers07@gmail.com>
2024-01-17 13:07:39 -0800

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
+#!/usr/bin/python3
 def islower(c):
-    # Check if the ASCII value of the character is between the ASCII values of 'a' and 'z'
-    return ord('a') <= ord(c) <= ord('z')
-
-# Example usage:
-char = 'a'
-result = islower(char)
-print(result)  # Output: True
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
