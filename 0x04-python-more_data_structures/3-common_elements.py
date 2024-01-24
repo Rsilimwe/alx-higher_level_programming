2391ba19ad4db6e88b0555445ceb8d357003e58c
Rsilimwe <Silimwerodgers07@gmail.com>
2024-01-24 07:06:11 -0800

    3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# ----------------------------------------
+# Python program that demonstrates:
+#  a function that returns a set of common elements in two sets.
+# 
+# (C) 2024 rodgers Silimwe, Senanga, Zambia 
+# Email silimwerodgers@gmail.com
+# ---------------------------------------------------
+# 
+# 
+
+def common_elements(set_1, set_2):
+    return set_1 & set_2
